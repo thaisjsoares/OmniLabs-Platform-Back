63b8a29f4a4326df25492951c5720781d651e1e1
herick <herick@pop-os.localdomain>
2021-03-12 23:08:25 -0300

    ✅ test: add list courses test

---
+++ src/modules/courses/services/ShowCoursesService.spec.ts
@@ -0,0 +1,34 @@
+import AppError from '../../../shared/errors/AppError';
+import FakeCoursesRepository from '@modules/courses/repositories/fakes/FakeCoursesRepository'
+import ShowCoursesService from '../services/ShowCoursesService';
+
+let fakeCoursesRepository: FakeCoursesRepository;
+let showCourses: ShowCoursesService;
+
+describe('CreateCourse', () => {
+    beforeEach(() => {
+        fakeCoursesRepository = new FakeCoursesRepository();
+
+        showCourses = new ShowCoursesService(
+            fakeCoursesRepository
+        );
+    })
+
+    it('should be able to list courses', async () => {
+        const course1 = await fakeCoursesRepository.create({
+            name: "starter",
+            description: "asdasd"
+        })
+
+        const course2 = await fakeCoursesRepository.create({
+            name: "explorer",
+            description: "123123"
+        })
+
+        const courses = await showCourses.execute();
+
+        expect(courses).toEqual([course1, course2])
+    })
+
+    
+})
