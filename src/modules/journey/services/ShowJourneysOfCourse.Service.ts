1af64a03c38ac1d0fde3eaf6b423fd5d756b2dad
herick <herick@pop-os.localdomain>
2021-03-13 20:37:58 -0300

    :white_check_mark: test: add test to show journeys of course

--- src/modules/journey/services/ShowJourneysOfCourse.Service.ts
+++ src/modules/journey/services/ShowJourneysOfCourse.Service.ts
@@ -9,15 +9,12 @@ import Journey from '../infra/typeorm/entities/Journey';
 class ShowJourneysService {
     constructor(
         @inject('JourneyRepository')
-        private journeyRepository: IJourneyRepository,
+        private journeyRepository: IJourneyRepository
+
     ) {}
 
     public async execute(course_id: string): Promise<Journey[]> {
         const journeys = await this.journeyRepository.findByCourseId(course_id);
-
-        if(!journeys) {
-            throw new AppError('Impossible to find Journeys');
-        }
 
         return journeys;
     }
