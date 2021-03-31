fd1ad7d9b713c2d0d271f66247e464ad51f1813d
herick <herick@pop-os.localdomain>
2021-03-30 23:14:45 -0300

    :bento: fix: update eslint

--- src/modules/roles/useCases/createRole/CreateRoleController.spec.ts
+++ src/modules/roles/useCases/createRole/CreateRoleController.spec.ts
@@ -1,10 +1,10 @@
+import { hash } from 'bcryptjs';
 import request from 'supertest';
+import { Connection } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
 import { app } from '@shared/infra/http/app';
-import { Connection } from 'typeorm';
 import createConnection from '@shared/infra/typeorm/index';
-import { hash } from 'bcryptjs';
 
 let connection: Connection;
 
@@ -15,6 +15,24 @@ describe('Create Role Controller', async () => {
         const id = uuid();
 
         const password = await hash('admin', 8);
+
+        await connection.query(
+            `INSERT INTO ROLES(id, name, created_at, updated_at)
+                values('acbdc058-fbe8-45b2-9b9e-9f65c35a6987','Admin', 'now()', 'now()' )
+            `,
+        );
+
+        await connection.query(
+            `INSERT INTO USERS(id, name, email, password, created_at, updated_at)
+                values('8e3b0d9d-925e-4be1-b440-e4ccb41f8657','admin', 'admin@omnilabs.com.br', '${password}', 'now()', 'now()' )
+            `,
+        );
+
+        await connection.query(
+            `INSERT INTO users_roles(id, user_id, created_at, updated_at, role_id )
+                values('${id}','8e3b0d9d-925e-4be1-b440-e4ccb41f8657', 'now()', 'now()', 'acbdc058-fbe8-45b2-9b9e-9f65c35a6987')
+            `,
+        );
     });
     it('shoud be able to create a new role', async () => {
         const response = await request(app).post('/roles').send({
--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -1,5 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import { hash } from 'bcrypt';
+
 import getConnection from '..';
 
 async function create() {
