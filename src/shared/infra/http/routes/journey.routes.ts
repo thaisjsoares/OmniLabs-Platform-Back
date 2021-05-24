04bd52ae92fcb4f0ff3d2b34956037a3f61811db
herick <herick@pop-os.localdomain>
2021-05-24 19:05:28 -0300

    feat(journey): add route to list journeys of course name

--- src/shared/infra/http/routes/journey.routes.ts
+++ src/shared/infra/http/routes/journey.routes.ts
@@ -3,6 +3,7 @@ import CreateJourneyController from '@modules/journey/useCases/CreateJourney/Cre
 import RemoveJourneyController from '@modules/journey/useCases/RemoveJourney/RemoveJourneyController';
 import ShowAllJourneyController from '@modules/journey/useCases/ShowAllJourney/ShowAllJourneyController';
 import ShowJounreysOfCourseController from '@modules/journey/useCases/ShowJourneysOfCourse/ShowJounreysOfCourseController';
+import ShowJourneysOfCourseNameController from '@modules/journey/useCases/ShowJourneysOfCourseName/ShowJourneysOfCourseNameController';
 import UpdateJourneyController from '@modules/journey/useCases/UpdateJourney/UpdateJourneyController';
 import UpdateJourneyImageController from '@modules/journey/useCases/UpdateJourneyImage/UpdateJourneyImageController';
 import { Router } from 'express';
@@ -16,6 +17,7 @@ const showAllJourneysController = new ShowAllJourneyController();
 const showJourneysOfCourseController = new ShowJounreysOfCourseController();
 const updateJourneyController = new UpdateJourneyController();
 const updateJourneyImageController = new UpdateJourneyImageController();
+const showJourneysOfCourseNameController = new ShowJourneysOfCourseNameController();
 const upload = multer(uploadConfig.multer);
 
 journeyRouter.post('/', createJourneyController.handle);
@@ -23,6 +25,11 @@ journeyRouter.get('/:course_id', showJourneysOfCourseController.handle);
 journeyRouter.get('/', showAllJourneysController.handle);
 journeyRouter.delete('/:journey_id', removeJourneyController.handle);
 journeyRouter.put('/:journey_id', updateJourneyController.handle);
+
+journeyRouter.get(
+    '/course/:course_name',
+    showJourneysOfCourseNameController.handle,
+);
 
 journeyRouter.patch(
     '/image/:journey_id',
